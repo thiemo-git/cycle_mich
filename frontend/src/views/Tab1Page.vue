a33970093136d5e68cbc61c22d2d0f90411ebacd
Andreas May <andi.m@wunderbit.de>
2024-09-07 13:31:54 +0200

    frontend levelup

--- frontend/src/views/Tab1Page.vue
+++ frontend/src/views/Tab1Page.vue
@@ -2,7 +2,7 @@
   <ion-page>
     <ion-header>
       <ion-toolbar>
-        <ion-title>Tab 1</ion-title>
+        <ion-title>Chatbot</ion-title>
       </ion-toolbar>
     </ion-header>
     <ion-content :fullscreen="true">
@@ -12,7 +12,11 @@
         </ion-toolbar>
       </ion-header>
 
-      <ExploreContainer name="Tab 1 page" />
+
+      <div id="container">
+        <ion-button @click="takePhoto">Foto aufnehmen</ion-button>
+        <img v-if="photo" :src="'data:image/jpeg;base64,' + photo" />
+      </div>
     </ion-content>
   </ion-page>
 </template>
@@ -20,4 +24,36 @@
 <script setup lang="ts">
 import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/vue';
 import ExploreContainer from '@/components/ExploreContainer.vue';
+</script>
+
+<script  lang="ts">
+import { Camera, CameraResultType, CameraSource } from '@capacitor/camera';
+
+export default {
+  name: 'Home',
+  data() {
+    return {
+      photo: null, // Speichert das Base64-Foto
+    };
+  },
+  methods: {
+    async takePhoto() {
+      try {
+        const image = await Camera.getPhoto({
+          quality: 90,
+          allowEditing: false,
+          resultType: CameraResultType.Base64,
+          source: CameraSource.Camera,
+        });
+
+        this.photo = image.base64String;
+        console.log(image.base64String);
+        alert(import.meta.env.VITE_OPENAI_KEY);
+
+      } catch (error) {
+        console.error('Error taking photo', error);
+      }
+    },
+  },
+};
 </script>
