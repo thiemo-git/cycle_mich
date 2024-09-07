c5510024de6ce945f21a85fa6e7d11e37066dd1e
Thiemo <thiemo238@gmail.com>
2024-09-07 19:44:20 +0200

    remove data key

--- backend/app/Http/Resources/BarcodeResource.php
+++ backend/app/Http/Resources/BarcodeResource.php
@@ -7,6 +7,8 @@ use Illuminate\Http\Resources\Json\JsonResource;
 
 class BarcodeResource extends JsonResource
 {
+    public static $wrap = null;
+
     /**
      * Transform the resource into an array.
      *
