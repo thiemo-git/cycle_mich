831feebeeaf08235d17e98e0871a95d19b8fc84a
Thiemo <thiemo238@gmail.com>
2024-09-07 12:16:52 +0200

    deaktiviert Autoincrement bei der ID

--- backend/database/migrations/2024_09_07_093312_create_barcodes_table.php
+++ backend/database/migrations/2024_09_07_093312_create_barcodes_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('barcodes', function (Blueprint $table) {
-            $table->id();
+            $table->unsignedBigInteger('id')->primary()->unique();
             $table->string('title', 50);
             $table->text('notes')->nullable();
             $table->timestamps();
